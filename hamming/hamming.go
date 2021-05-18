19286dedcc418c258cc3c24df8d2ab9bb477b596
Joel Kaplan <joelkaplan1@gmail.com>
2021-05-18 11:58:44 -0400

    Update solution;

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -5,19 +5,14 @@ import "errors"
 // Distance returns the Hamming distance between two strings
 // It returns the distance as an int or an error
 func Distance(a, b string) (int, error) {
-	dist := 0
-	bLen := len(b)
-	for idx, letter := range a {
-		if (idx + 1) > bLen {
-			dist++
-		} else {
-			if string(letter) != string(b[idx]) {
-				dist++
-			}
-		}
-	}
 	if len(b) != len(a) {
-		return 0, errors.New("Length erro")
+		return 0, errors.New("Strings are of different lengths")
+	}
+	dist := 0
+	for idx, letter := range a {
+		if string(letter) != string(b[idx]) {
+			dist++
+		}
 	}
 	return dist, nil
 }
