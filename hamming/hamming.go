2ae648f6b7bb47aa5ee219cd7919929bf98664ba
Joel Kaplan <joelkaplan1@gmail.com>
2021-05-19 15:13:37 -0400

    Convert string to slice of runes first

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -5,12 +5,13 @@ import "errors"
 // Distance returns the Hamming distance between two strings
 // It returns the distance as an int or an error
 func Distance(a, b string) (int, error) {
-	if len(b) != len(a) {
+	ar, br := []rune(a), []rune(b)
+	if len(br) != len(ar) {
 		return 0, errors.New("Strings are of different lengths")
 	}
 	dist := 0
-	for idx, letter := range a {
-		if string(letter) != string(b[idx]) {
+	for idx := range ar {
+		if ar[idx] != br[idx] {
 			dist++
 		}
 	}
