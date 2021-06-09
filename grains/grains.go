1762f132d7494aeb533a319f29d7634f64fe5717
Joel <joelkaplan1@gmail.com>
2021-06-09 13:58:29 -0400

    Complete bit shift problem

--- grains/grains.go
+++ grains/grains.go
@@ -1,16 +1,23 @@
 package grains
 
+import "errors"
+
 func Total() uint64 {
-	var sum uint64
-	sum = 1
-	for i:=1;i<=64;i++{
-		sum*=2
+	var sum uint64 = 1
+	// sum = 1
+	for i := 1; i <= 64; i++ {
+		// sum*=2
+		sum = sum << 1
+		// fmt.Println(sum)
 	}
-	return sum
+	// fmt.Println(sum)
+	return sum - 1
 }
 
 func Square(sq int) (uint64, error) {
-	var value uint64
-	value = 1
-	return value, nil
+	var value uint64 = 1
+	if sq <= 0 || sq > 64 {
+		return 0, errors.New("Error")
+	}
+	return value << uint64(sq-1), nil
 }
