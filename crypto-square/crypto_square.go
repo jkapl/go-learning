df8c3033309f178037b657f7c4a1c411a066227d
Joel <joelkaplan1@gmail.com>
2021-06-11 17:56:43 -0400

    Start crypto square;

---
+++ crypto-square/crypto_square.go
@@ -0,0 +1,39 @@
+package cryptosquare
+
+import ( 
+	"unicode"
+	"fmt"
+	"math"
+)
+
+func Encode(s string) string {
+	var normalized []rune
+	var final []rune
+	for _, val := range s {
+		if unicode.IsLetter(val) {
+			normalized = append(normalized, unicode.ToLower(val))
+		}
+	}
+	fmt.Println(normalized)
+	var sq = math.Sqrt(float64(len(s)))
+	var c float64
+	var r float64
+	var diff = sq - math.Round(sq)
+	if diff != 0 {
+		if diff > 0.5 {
+			c = math.Round(sq)
+			r = math.Round(sq) - 1
+		} else {
+			c = math.Round(sq) + 1
+			r = math.Round(sq)
+		}
+	} else {
+		c = sq
+		r = sq
+	} 
+	for i, val := range normalized {
+
+	}
+
+
+}
