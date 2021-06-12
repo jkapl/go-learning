06b33e81fae3148d6a29812bf9de87068aae896d
Joel <joelkaplan1@gmail.com>
2021-06-11 20:41:16 -0400

    Crypto square attempt

--- crypto-square/crypto_square.go
+++ crypto-square/crypto_square.go
@@ -2,7 +2,7 @@ package cryptosquare
 
 import ( 
 	"unicode"
-	"fmt"
+	// "fmt"
 	"math"
 )
 
@@ -10,30 +10,45 @@ func Encode(s string) string {
 	var normalized []rune
 	var final []rune
 	for _, val := range s {
-		if unicode.IsLetter(val) {
+		if unicode.IsLetter(val) || unicode.IsNumber(val) {
 			normalized = append(normalized, unicode.ToLower(val))
 		}
 	}
-	fmt.Println(normalized)
 	var sq = math.Sqrt(float64(len(s)))
-	var c float64
-	var r float64
+	var c int
+	// var r int
 	var diff = sq - math.Round(sq)
 	if diff != 0 {
 		if diff > 0.5 {
-			c = math.Round(sq)
-			r = math.Round(sq) - 1
+			c = int(math.Round(sq))
+			// r = int(math.Round(sq) - 1)
 		} else {
-			c = math.Round(sq) + 1
-			r = math.Round(sq)
+			c = int(math.Round(sq) + 1)
+			// r = int(math.Round(sq))
 		}
 	} else {
-		c = sq
-		r = sq
+		c = int(sq)
+		// r = int(sq)
 	} 
-	for i, val := range normalized {
-			
+	var ptr = 0
+	var col = 0
+	// var row = 0
+	var totalLength = len(normalized)
+	for totalLength % c != 0 {
+		totalLength += 1
 	}
-
-
+	for i := 0; i < totalLength; i++ {
+		// if i > len(normalized) {
+		// 	final = append(final, ' ')
+			
+		// }
+		if ptr >= len(normalized) {
+			col += 1
+			ptr = col
+			final = append(final, ' ')
+		}
+		final = append(final, normalized[ptr])
+		ptr += c
+	}
+	return string(final)
 }
