705555812a3c7b0d1459d43b4f5e477d9eafc626
Joel <joelkaplan1@gmail.com>
2021-06-14 10:05:26 -0400

    Simplify grains

--- grains/grains.go
+++ grains/grains.go
@@ -4,15 +4,14 @@ import "errors"
 
 // Total returns 2^64 using bit manipulation
 func Total() uint64 {
-	var sum uint64 = 1
-	return ((sum << 63) * 2) - 1
+	return 1<<64 - 1
 }
 
 // Square returns 2^n where n is the input
 func Square(sq int) (uint64, error) {
-	var value uint64 = 1
+	// var value uint64 = 1
 	if sq <= 0 || sq > 64 {
 		return 0, errors.New("Error")
 	}
-	return value << uint64(sq-1), nil
+	return 1 << (sq - 1), nil
 }
