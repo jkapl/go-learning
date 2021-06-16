d703c5f061a9e202954e6a00edbde89e57b348de
Joel <joelkaplan1@gmail.com>
2021-06-16 15:16:05 -0400

    Write function signatures for clock

---
+++ clock/clock.go
@@ -0,0 +1,21 @@
+package clock
+
+type Clock struct {
+	
+}
+
+func New(hour, minute int) Clock {
+
+}
+
+func (c Clock) String() string {
+
+}
+
+func (c Clock) Add(minutes int) Clock {
+
+}
+
+func (c Clock) Substract(minutes int) Clock {
+
+}
