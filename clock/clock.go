c2f54d49b788fb79c1b5c1f9f891e5e101aa5a7d
Joel <joelkaplan1@gmail.com>
2021-06-21 21:50:03 -0400

    Continue clock

--- clock/clock.go
+++ clock/clock.go
@@ -1,21 +1,34 @@
 package clock
 
+import "math"
+
 type Clock struct {
-	
+	hour, minute int
 }
 
 func New(hour, minute int) Clock {
+	totalMin := (hour * 60) + minute 
+	if totalMin < 0 {
 
+	}
+	hours := int(math.Floor(float64(totalMin) / 60))
+	mins := int(totalMin % 60)
+	
+	c := Clock{hours, mins}
+	return c
+	
 }
 
 func (c Clock) String() string {
-
+	return string(c.hour) + ":" + string(c.minute)
 }
 
 func (c Clock) Add(minutes int) Clock {
-
+	// if minutes >= 60
+	return Clock{1,1}
 }
 
-func (c Clock) Substract(minutes int) Clock {
+func (c Clock) Subtract(minutes int) Clock {
 
+	return Clock{1,1}
 }
